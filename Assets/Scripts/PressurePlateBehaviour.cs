71300e676c414e9a71d292ad042b4b44c51f7d68
carcaAntoine <reseauantoine@gmail.com>
2023-01-30 20:21:42 +0100

    Polish + maping + code pressure plate

---
+++ Assets/Scripts/PressurePlateBehaviour.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlateBehaviour : MonoBehaviour
+{
+
+    //########## Script qui permet d'abaisser une plateforme lorsque l'on marche sur la plaque de pression ##########
+
+    public GameObject elementToMove; //Element à déplacer pour ouvrir le chemin
+    
+    public float yValue; //Valeur Y d'arrivée d'elementToMove
+    
+    public Light lt;
+    Color redColor = Color.red;
+    Color greenColor = Color.green;
+    bool canBeActivated = true; //pour s'assurer qu'on ne peut appuyer sur la plaque de pression qu'une fois
+    float actualX, actualY, actualZ; //Coordonnées d'origine de la plateforme 
+
+    void Start()
+    {
+        lt.color = redColor;
+        actualX = elementToMove.transform.position.x;
+        actualY = elementToMove.transform.position.y;
+        actualZ = elementToMove.transform.position.z;
+    }
+
+    void OnTriggerEnter()
+    {
+        if (canBeActivated)
+        {
+            StartCoroutine(OpenPath());
+            lt.color = greenColor;
+            canBeActivated = false;
+        }
+
+    }
+
+    IEnumerator OpenPath()
+    {
+        for(float y = actualY; y > yValue; y -= 0.1f)
+        {
+            elementToMove.transform.position = new Vector3(actualX, y, actualZ);
+            yield return null;
+        }
+    }
+}
